95fdee231ff0dbc486e6b4925226066d3ef889c3
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-17 08:26:08 -0600

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Users extends Component {
-
   render() {
     return (
       <div>
         <ul>
-          Users!
+          {this.props.users.map((user, idx) => {
+            return (
+              <li key={idx}>
+                {idx + 1}. {user.username} - {user.hometown}
+              </li>
+            );
+          })}
         </ul>
       </div>
-    )
+    );
   }
 }
 
 //add mapStateToProps here
+const mapStateToProps = state => {
+  return { users: state.users };
+};
 
-export default Users
+export default connect(mapStateToProps)(Users);
